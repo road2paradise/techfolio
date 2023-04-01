c40f965a351657838862717dd5fa97849d2cb28d
road2paradise <ken_nggg@outlook.com>
2023-04-01 18:02:38 +1300

    Add contentful client

--- MyCoolWebAPI/Program.cs
+++ MyCoolWebAPI/Program.cs
@@ -1,8 +1,18 @@
+using Contentful.AspNetCore;
+
+// Builder and configurations
 var builder = WebApplication.CreateBuilder(args);
+var _configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true).Build();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddContentful(_configuration)
+    .AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
