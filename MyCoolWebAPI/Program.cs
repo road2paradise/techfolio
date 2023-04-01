6324dc61efd86fe64bff730f9892c7307bc66457
road2paradise <ken_nggg@outlook.com>
2023-04-01 23:31:31 +1300

    Add lambda libraries

--- MyCoolWebAPI/Program.cs
+++ MyCoolWebAPI/Program.cs
@@ -16,6 +16,7 @@ builder.Services
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
