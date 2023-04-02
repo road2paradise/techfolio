37d4bc22f5efc1c36742c3e81bfebc85e540bc59
road2paradise <ken_nggg@outlook.com>
2023-04-02 17:24:34 +1200

    Create IoC for contentful and inject stored secrets

--- MyCoolWebAPI/IoC/Contentful.cs
+++ MyCoolWebAPI/IoC/Contentful.cs
@@ -1,10 +1,26 @@
-﻿namespace MyCoolWebAPI.IoC
+﻿using Contentful.AspNetCore;
+using Contentful.Core;
+using Contentful.Core.Configuration;
+
+namespace MyCoolWebAPI.IoC
 {
     public static class Contentful
     {
         public static IServiceCollection AddContentfulClient(this IServiceCollection services, IConfiguration _configuration)
         {
-            // fetch from secrets here
+            ContentfulOptions options = new ContentfulOptions
+            {
+                SpaceId = _configuration.GetValue<string>("spaceId"),
+                ManagementApiKey = _configuration.GetValue<string>("managementapikey"),
+                PreviewApiKey = _configuration.GetValue<string>("previewapikey"),
+                DeliveryApiKey = _configuration.GetValue<string>("deliveryapikey")
+            };
+            
+            services.AddTransient<IContentfulClient, ContentfulClient>((ip) =>
+            {
+                var client = ip.GetService<HttpClient>();
+                return new ContentfulClient(client, options);
+            });
             return services;
         }
     }
