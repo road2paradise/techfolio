f3950fd9ef19f0c021021d00fb0bdab16d4fb0cd
road2paradise <ken_nggg@outlook.com>
2023-06-27 01:29:39 +1200

    Yolo deploy

--- _spa/src/index.tsx
+++ _spa/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { AppRoot } from './AppRoot';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AppRoot />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ _spa/src/themes/themes.js
@@ -0,0 +1,11 @@
+export const light = {
+    palette: {
+      mode: "light",
+    },
+};
+  
+export const dark = {
+    palette: {
+      mode: "dark",
+    },
+};
