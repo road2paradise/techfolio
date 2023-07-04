97b93c45487aa85ae9a74c4b35c78a05f98c082c
road2paradise <ken_nggg@outlook.com>
2023-07-04 17:02:43 +1200

    Remove more unused imports

--- _spa/src/AppRoot.tsx
+++ _spa/src/AppRoot.tsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchAssets, fetchWebsiteBodyText, fetchWorkExperience, selectContent, selectProfilePicture } from './slices/content.slice';
 import { AppDispatch } from './store/store';
 import IntroductionSection from './components/Sections/IntroductionSection/IntroductionSection';
-import { Link } from 'react-scroll'
 import { FooterSection } from './components/Sections/FooterSection/FooterSection';
 import { WorkExperienceSection } from './components/Sections/WorkExperienceSection/WorkExperienceSection';
 import { ColorRing } from 'react-loader-spinner';
